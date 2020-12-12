5e74a9c7b1e853daae94c288a5d1648294457d12
Erwan <erwanletutour.elt@gmail.com>
2020-12-12 10:04:34 +0100

    Advent Of Code 2020 Day 12 Java

--- Java/2020/2020-Jour12/src/main/java/Direction.java
+++ Java/2020/2020-Jour12/src/main/java/Direction.java
@@ -13,10 +13,6 @@ public enum Direction {
         this.code = code;
     }
 
-    public static Direction getByDirCode(char code) {
-        return Arrays.stream(values()).filter(e -> e.code == code).findAny().get();
-    }
-
     public static Direction getByDir(char code) {
         return Arrays.stream(values()).filter(e -> e.name().charAt(0) == code).findAny().get();
     }
@@ -40,49 +36,6 @@ public enum Direction {
             case WESTNORTH: return new Point(currentLocation.x-amount, currentLocation.y-amount);
         }
         throw new IllegalStateException("Non-existent Direction: "+this);
-    }
-
-    public char getInGrid(char[][] grid, Point p, char none){
-        if(p.x>=0 && p.x<grid.length && p.y>=0 && p.y<grid[0].length){
-            return grid[p.x][p.y];
-        }
-        return none;
-    }
-
-    public char getInGrid(char[][] grid, Point p){
-        return getInGrid(grid, p, '.');
-    }
-
-    public Point move(Point currentLocation) {
-        return move(currentLocation, 1);
-    }
-
-    public Direction opposite() {
-        switch (this) {
-            case NORTH: return SOUTH;
-            case SOUTH: return NORTH;
-            case EAST: return WEST;
-            case WEST: return EAST;
-            case NORTHEAST: return SOUTHWEST;
-            case SOUTHWEST: return NORTHEAST;
-            case EASTSOUTH: return WESTNORTH;
-            case WESTNORTH: return EASTSOUTH;
-        }
-        throw new IllegalStateException("Non-existent Direction: "+this);
-    }
-
-    public static Direction getByMove(Point from, Point to) {
-        if(to.x > from.x) return EAST;
-        else if(to.x < from.x) return WEST;
-        else if(to.y > from.y) return SOUTH;
-        else if(to.y < from.y) return NORTH;
-        throw new IllegalStateException("From and to location are the same: "+from+", "+to);
-    }
-
-    public boolean leftOf(Direction robotDir) {
-        int n = this.ordinal()-1;
-        if(n == -1) n = values().length-1;
-        return robotDir.ordinal() == n;
     }
 
     public static Direction[] fourDirections(){
