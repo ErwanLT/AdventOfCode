ce5624cf676959760f69a737ec1cd3140f81f888
Erwan <erwanletutour.elt@gmail.com>
2020-12-14 16:43:08 +0100

    add custom printer

--- Java/2020/src/main/java/MyPrinter.java
+++ Java/2020/src/main/java/MyPrinter.java
@@ -5,6 +5,11 @@ import java.time.temporal.ChronoUnit;
 
 public class MyPrinter extends PrintStream {
 
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_CYAN = "\u001B[36m";
 
     public MyPrinter(OutputStream out) {
         super(out);
@@ -12,6 +17,18 @@ public class MyPrinter extends PrintStream {
 
     @Override
     public void println(String string) {
-        super.println("[" + Instant.now().truncatedTo(ChronoUnit.MILLIS).toString().replaceAll("[TZ]", " ") + "] " + string);
+        super.println(ANSI_CYAN + "[" + Instant.now().truncatedTo(ChronoUnit.MILLIS).toString().replaceAll("[TZ]", " ") + "] " + ANSI_RESET + string);
+    }
+
+    public void printlnRed(String string){
+        println(ANSI_RED + string + ANSI_RESET);
+    }
+
+    public void printlnGreen(String string){
+        println(ANSI_GREEN + string + ANSI_RESET );
+    }
+
+    public void printlnBlue(String string){
+        println(ANSI_BLUE + string + ANSI_RESET);
     }
 }
