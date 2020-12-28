dd40f48938cf39a0329c5afa44ac598fa335c92a
Erwan <erwanletutour.elt@gmail.com>
2020-12-28 13:46:47 +0100

    Advent of Code Java 2015 Day 7

---
+++ Java/2015/2015-jour7/src/main/java/Jour7.java
@@ -0,0 +1,155 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Jour7 {
+
+    private static List<String> input;
+
+    static Function<String, Integer> notOperation = s -> (~getValue(s));
+
+    static BiFunction<String, String, Integer> andOperation = (arg1, arg2) -> (getValue(arg1) & getValue(arg2));
+
+    static BiFunction<String, String, Integer> orOperation = (arg1, arg2) -> (getValue(arg1) | getValue(arg2));
+
+    static BiFunction<String, String, Integer> lShiftOperation = (arg1, arg2) -> (getValue(arg1) << getValue(arg2));
+
+    static BiFunction<String, String, Integer> rShiftOperation = (arg1, arg2) -> (getValue(arg1) >> getValue(arg2));
+
+    private static final Pattern singleArgumentInstructionPattern = Pattern.compile("(.+) (.+)");
+
+    private static final Pattern doubleArgumentInstructionPattern = Pattern.compile("(.+) (.+) (.+)");
+
+    private static int part1Solution = 0;
+
+    public static void main(String[] args) throws URISyntaxException {
+        System.out.println("------ Start ------");
+
+        getFileInput();
+
+        initCircuit();
+        solvePart1();
+
+        initCircuit();
+        solvePart2();
+
+        System.out.println("------ End ------");
+    }
+
+    private static void getFileInput() throws URISyntaxException {
+        System.out.println("------ Getting input : Start ------");
+        Jour7 app = new Jour7();
+        String fileName = "input";
+        File file = app.getFileFromResource(fileName);
+        getFileLine(file);
+        System.out.println("------ Getting input : End ------");
+    }
+
+    private static void getFileLine(File file) {
+        try {
+            input = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private File getFileFromResource(String fileName) throws URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
+    }
+
+    private static Map<String, String> circuit = new HashMap<>();
+
+    private static void initCircuit() {
+        circuit.clear();
+        input.stream().map(str -> str.split(" -> ")).forEach(str -> setValue(str[1], str[0]));
+    }
+
+    protected static void setValue(String key, String instruction) {
+        circuit.put(key, instruction);
+    }
+
+    private static int getValue(String arg) {
+        int value = 0;
+        if (arg.matches("\\d+")) {
+            value = Integer.valueOf(arg);
+        } else {
+            value = processWiringInstruction(circuit.get(arg));
+            circuit.put(arg, String.valueOf(value));
+        }
+        return value;
+    }
+
+    public static int processWiringInstruction(String instruction) {
+        Matcher matcher = doubleArgumentInstructionPattern.matcher(instruction);
+        Matcher matcher1 = singleArgumentInstructionPattern.matcher(instruction);
+        int result = 0;
+        if (matcher.find()) {
+            String arg1 = matcher.group(1);
+            String operation = matcher.group(2);
+            String arg2 = matcher.group(3);
+            switch (operation) {
+                case "AND":
+                    result = andOperation.apply(arg1, arg2);
+                    break;
+                case "OR":
+                    result = orOperation.apply(arg1, arg2);
+                    break;
+                case "LSHIFT":
+                    result = lShiftOperation.apply(arg1, arg2);
+                    break;
+                case "RSHIFT":
+                    result = rShiftOperation.apply(arg1, arg2);
+                    break;
+                default:
+                    break;
+            }
+        } else if (matcher1.find()) {
+            String operation = matcher1.group(1);
+            String arg1 = matcher1.group(2);
+            if (operation.equals("NOT")) {
+                result = notOperation.apply(arg1);
+            }
+        } else {
+            String arg1 = instruction;
+            result = getValue(arg1);
+        }
+        return result;
+    }
+
+
+    private static void solvePart1() {
+        System.out.println("------ Part1 : Start ------");
+
+        part1Solution = getValue("a");
+        System.out.println("The signal ultimately provided to wire a is : " + part1Solution);
+
+        System.out.println("------ Part1 : End ------");
+    }
+
+
+    private static void solvePart2() {
+        System.out.println("------ Part2 : Start ------");
+
+        circuit.put("b", String.valueOf(part1Solution));
+        int part2Solution = getValue("a");
+        System.out.println("The new signal ultimately provided to wire a is : " + part2Solution);
+
+        System.out.println("------ Part2 : End ------");
+    }
+}
--- Java/2015/README.md
+++ Java/2015/README.md
@@ -2,8 +2,8 @@
 
 [![About](https://img.shields.io/badge/Advent%20of%20Code-2015-brightgreen)](https://adventofcode.com/2015)
 [![Language](https://img.shields.io/badge/Language-Java-orange)](https://www.java.com/)
-[![Days completed](https://img.shields.io/badge/Days%20completed-6-blue)]()
-[![Stars](https://img.shields.io/badge/⭐️-12-yellow)]()
+[![Days completed](https://img.shields.io/badge/Days%20completed-7-blue)]()
+[![Stars](https://img.shields.io/badge/⭐️-14-yellow)]()
 
 * [Day 1](2015-jour1/README.md)
 * [Day 2](2015-jour2/README.md)
@@ -11,3 +11,4 @@
 * [Day 4](2015-jour4/README.md)
 * [Day 5](2015-jour5/README.md)
 * [Day 6](2015-jour6/README.md)
+* [Day 7](2015-jour7/README.md)
